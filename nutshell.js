c4c41ba92cdaf2c19c254148da4643bc46dd9ba0
Nicky Case <n@ncase.me>
2022-08-25 12:14:48 -0400

    fix youtube bug, refactor to be tidier

--- nutshell.js
+++ nutshell.js
@@ -703,19 +703,15 @@ Bubble: the box that expands below an expandable, containing a Nutshell Section
             }else if(_isYouTube(url)){
 
                 // Get the video ID - youtube.com or youtu.be
-                let videoID;
-                if(url.indexOf("youtube.com")>=0){
-                    videoID = url.split("v=")[1];
-                }else if(url.indexOf("youtu.be")>=0){
-                    videoID = url.split("be/")[1];
+                // and other URL params like time.
+                url = new URL(url);
+                let videoID, t;
+                if( url.host.indexOf("youtube.com") >= 0 ){
+                    videoID = url.searchParams.get('v');
+                }else if( url.host.indexOf("youtu.be") >= 0 ){
+                    videoID = url.pathname.slice(1);
                 }
-                if(videoID.indexOf("?")>=0){ // cut out other params
-                    videoID = videoID.split("?")[0];
-                }
-
-                // Any URL params... like time
-                let urlParams = new URL(url),
-                    t = urlParams.searchParams.get("t") || urlParams.searchParams.get("start") || '0';
+                t = parseInt( url.searchParams.get("t") || url.searchParams.get("start") || '0' );
 
                 // Gimme, easy peasy.
                 // weird css hack to make the iframe scale aspect-ratio.
