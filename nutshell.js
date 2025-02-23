6641b7a660b317f9e2a51d8a2cb1e193beaa5daf
FireIsGood <fireisadequate@gmail.com>
2025-02-23 02:26:23 -0800

    Fix ball color being static

--- nutshell.js
+++ nutshell.js
@@ -652,11 +652,6 @@ Bubble: the box that expands below an expandable, containing a Nutshell Section
             ex.appendChild(ballUp);
             ex.appendChild(ballDown);
 
-            // BALLS ARE SAME AS FONT COLOR
-            let linkStyle = window.getComputedStyle(forThisElement ? forThisElement : ex);
-            ballUp.style.background = linkStyle.color;
-            ballDown.style.background = linkStyle.color;
-
             // Save the punctuation!
             // Extremely inefficient: plop each character one-by-one into the span
             let punctuation = document.createElement('span');
@@ -1971,7 +1966,7 @@ Bubble: the box that expands below an expandable, containing a Nutshell Section
         /* They're balls */
         width: 0.15em;
         height: 0.15em;
-        background: #000;
+        background: currentcolor;
         border-radius: 1em;
 
         /* Animate moving up & down */
